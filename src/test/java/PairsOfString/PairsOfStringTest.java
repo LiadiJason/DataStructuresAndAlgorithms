e70b2afb2d6e613ba90e4774532c3f5671858e62
LiadiJason <liadijason@gmail.com>
2023-08-07 20:51:32 +0700

    add test case for method by using list as parameter and mock for list of string

--- src/test/java/PairsOfString/PairsOfStringTest.java
+++ src/test/java/PairsOfString/PairsOfStringTest.java
@@ -2,6 +2,9 @@ package PairsOfString;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 
@@ -15,7 +18,23 @@ public class PairsOfStringTest {
 
     @Test
     public void pairsOfStringInArrayDuplicateAllowed_Success() {
-        String result = pairsOfString.pairsOfStringInArrayDuplicateAllowed(mockListOfString());
+        String result = pairsOfString.pairsOfStringInArrayDuplicateAllowed(mockArrayOfString());
+
+        String expectedOutput = "[A,A]\n" +
+                "[A,B]\n" +
+                "[A,C]\n" +
+                "[B,A]\n" +
+                "[B,B]\n" +
+                "[B,C]\n" +
+                "[C,A]\n" +
+                "[C,B]\n" +
+                "[C,C]\n";
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void pairsOfStringInArrayDuplicateAllowedWithList_Success() {
+        String result = pairsOfString.pairsOfStringInArrayDuplicateAllowedWithList(mockListOfString());
 
         String expectedOutput = "[A,A]\n" +
                 "[A,B]\n" +
@@ -31,7 +50,23 @@ public class PairsOfStringTest {
 
     @Test
     public void pairsOfStringInArrayEnhancedFor_Success() {
-        String result = pairsOfString.pairsOfStringInArrayEnhancedFor(mockListOfString());
+        String result = pairsOfString.pairsOfStringInArrayEnhancedFor(mockArrayOfString());
+
+        String expectedOutput = "[A,A]\n" +
+                "[A,B]\n" +
+                "[A,C]\n" +
+                "[B,A]\n" +
+                "[B,B]\n" +
+                "[B,C]\n" +
+                "[C,A]\n" +
+                "[C,B]\n" +
+                "[C,C]\n";
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void pairsOfStringInArrayEnhancedForWithList_Success() {
+        String result = pairsOfString.pairsOfStringInArrayEnhancedForWithList(mockListOfString());
 
         String expectedOutput = "[A,A]\n" +
                 "[A,B]\n" +
@@ -47,7 +82,20 @@ public class PairsOfStringTest {
 
     @Test
     public void pairsOfStringInArrayDuplicateNotAllowed_Success() {
-        String result = pairsOfString.pairsOfStringInArrayDuplicateNotAllowed(mockListOfString());
+        String result = pairsOfString.pairsOfStringInArrayDuplicateNotAllowed(mockArrayOfString());
+
+        String expectedOutput = "[A,B]\n" +
+                "[A,C]\n" +
+                "[B,A]\n" +
+                "[B,C]\n" +
+                "[C,A]\n" +
+                "[C,B]\n";
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void pairsOfStringInArrayDuplicateNotAllowedWithList_Success() {
+        String result = pairsOfString.pairsOfStringInArrayDuplicateNotAllowedWithList(mockListOfString());
 
         String expectedOutput = "[A,B]\n" +
                 "[A,C]\n" +
@@ -60,7 +108,7 @@ public class PairsOfStringTest {
 
     @Test
     public void pairsOfStringInArrayDuplicateNotAllowedAndReverseNotAllowed_Success() {
-        String result = pairsOfString.pairsOfStringInArrayDuplicateNotAllowedAndReverseNotAllowed(mockListOfString());
+        String result = pairsOfString.pairsOfStringInArrayDuplicateNotAllowedAndReverseNotAllowed(mockArrayOfString());
 
         String expectedOutput = "[A,B]\n" +
                 "[A,C]\n" +
@@ -68,11 +116,29 @@ public class PairsOfStringTest {
         assertEquals(expectedOutput, result);
     }
 
-    public static String[] mockListOfString() {
+    @Test
+    public void pairsOfStringInArrayDuplicateNotAllowedAndReverseNotAllowedWithList_Success() {
+        String result = pairsOfString.pairsOfStringInArrayDuplicateNotAllowedAndReverseNotAllowedWithList(mockListOfString());
+
+        String expectedOutput = "[A,B]\n" +
+                "[A,C]\n" +
+                "[B,C]\n";
+        assertEquals(expectedOutput, result);
+    }
+
+    public static String[] mockArrayOfString() {
         String[] alphabetArray = new String[3];
         alphabetArray[0] = "A";
         alphabetArray[1] = "B";
         alphabetArray[2] = "C";
         return alphabetArray;
+    }
+
+    public static List<String> mockListOfString() {
+        List<String> alphabetList = new ArrayList<>();
+        alphabetList.add("A");
+        alphabetList.add("B");
+        alphabetList.add("C");
+        return alphabetList;
     }
 }
