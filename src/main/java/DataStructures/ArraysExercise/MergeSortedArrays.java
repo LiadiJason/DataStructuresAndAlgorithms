4bc5858a9321670f7ebfd18d04825832ce09340b
LiadiJason <liadijason@gmail.com>
2023-08-17 20:11:48 +0700

    New arrays solution and test

--- src/main/java/DataStructures/ArraysExercise/MergeSortedArrays.java
+++ src/main/java/DataStructures/ArraysExercise/MergeSortedArrays.java
@@ -1,8 +1,28 @@
 package DataStructures.ArraysExercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author LiadiJason
  * @version $Id: MergeSortedArrays.java, 2023-08-10:50 AM LiadiJason
  */
 public class MergeSortedArrays {
+    public List<Integer> mergeSortedArrays(List<Integer> firstList, List<Integer> secondList) {
+        List<Integer> result = new ArrayList<>();
+
+        int i = 0, j = 0, k = 0;
+
+        for (;i < firstList.size() && j < secondList.size(); k++) {
+            if (firstList.get(i) < secondList.get(j)) {
+                result.add(firstList.get(i));
+                i++;
+            } else {
+                result.add(secondList.get(j));
+                j++;
+            }
+        }
+
+        return result;
+    }
 }
---
+++ src/test/java/DataStructures/ArraysTest/MergeSortedArraysTest.java
@@ -0,0 +1,33 @@
+package DataStructures.ArraysTest;
+
+import DataStructures.ArraysExercise.MergeSortedArrays;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author LiadiJason
+ * @version $Id: MergeSortedArraysTest.java, 2023-08-10:56 AM LiadiJason
+ */
+public class MergeSortedArraysTest {
+
+    MergeSortedArrays mergeSortedArrays = new MergeSortedArrays();
+
+    @Test
+    public void reverseString_Success() {
+        List<Integer> result = mergeSortedArrays.mergeSortedArrays(mockFirstList(), mockSecondList());
+        List<Integer> expectedOutput = new ArrayList<>(Arrays.asList(0,3,4,4,6,30,31));
+        Assert.assertEquals(expectedOutput, result);
+    }
+
+    public static List<Integer> mockFirstList() {
+        return new ArrayList<>(Arrays.asList(0,3,4,31));
+    }
+
+    public static List<Integer> mockSecondList() {
+        return new ArrayList<>(Arrays.asList(4,6,30));
+    }
+}
